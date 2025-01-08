1f782a8e7312bbfe67c6f3c2d5063b9404de42de
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-01-08 09:51:48 +0800

    feat: cleanup stuff

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -33,18 +33,7 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
-    providers: [
-        DiscordProvider
-        /**
-         * ...add more providers here.
-         *
-         * Most other providers require a bit more work than the Discord provider. For example, the
-         * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-         * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-         *
-         * @see https://next-auth.js.org/providers/github
-         */
-    ],
+    providers: [DiscordProvider],
     adapter: DrizzleAdapter(db, {
         usersTable: users,
         accountsTable: accounts,
